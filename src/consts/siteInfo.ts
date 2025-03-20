4d682e14f350371356a4094eea347bc9fb78caa6
Jeremy Orellana <jejorm8@gmail.com>
2025-03-20 17:30:23 -0500

    Update titles for each page

--- src/consts/siteInfo.ts
+++ src/consts/siteInfo.ts
@@ -1,4 +1,4 @@
 export const siteInfo = {
-  title: "Catch 'em all! Browse, bookmark, and discover Pokémon with a sleek Astro, Tailwind, and Solid.js-powered app.",
+  title: "Pokémon | Website",
   description: 'A fast and responsive Pokémon web app built with Astro, Tailwind CSS, and Solid.js. Browse live Pokémon data from PokeAPI, use pagination, and bookmark your favorites with local storage.'
 }
